ddf3e19c0acecc69f73da20f1aeb698793deac28
Abhishek Jain <abhi@noobsol.com>
2014-12-04 13:00:27 +0530

    Initial Commit

---
+++ src/config.h
@@ -0,0 +1,10 @@
+#ifndef _TXMPP_CONFIG_H_
+#define _TXMPP_CONFIG_H_
+
+#define FEATURE_ENABLE_SSL
+#define HAVE_OPENSSL_SSL_H
+#define POSIX
+#define SSL_USE_OPENSSL
+#define USE_SSLSTREAM
+
+#endif  // _TXMPP_CONFIG_H_
